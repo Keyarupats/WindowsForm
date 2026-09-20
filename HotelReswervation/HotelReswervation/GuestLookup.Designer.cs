8bc15c1e5acc44fd8938a966ed08ece304c8b876
John Patrick C. Malapitan <User@Malapitan>
2026-09-20 22:34:56 +0800

    Guest Lookup UI created

---
+++ HotelReswervation/HotelReswervation/GuestLookup.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace HotelReswervation
+{
+    partial class GuestLookup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel3 = new Panel();
+            label2 = new Label();
+            panel6 = new Panel();
+            dgvRoomDirectory = new DataGridView();
+            label6 = new Label();
+            panel7 = new Panel();
+            label5 = new Label();
+            panel5 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            label7 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label8 = new Label();
+            label9 = new Label();
+            textBox4 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            panel3.SuspendLayout();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvRoomDirectory).BeginInit();
+            panel7.SuspendLayout();
+            panel5.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(128, 128, 255);
+            panel3.Controls.Add(label2);
+            panel3.Location = new Point(12, 63);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(333, 34);
+            panel3.TabIndex = 8;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(3, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(187, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Guests Profile Lookup";
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.FromArgb(192, 192, 255);
+            panel6.Controls.Add(dgvRoomDirectory);
+            panel6.Controls.Add(panel7);
+            panel6.Location = new Point(360, 63);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(758, 522);
+            panel6.TabIndex = 7;
+            // 
+            // dgvRoomDirectory
+            // 
+            dgvRoomDirectory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvRoomDirectory.Location = new Point(3, 40);
+            dgvRoomDirectory.Name = "dgvRoomDirectory";
+            dgvRoomDirectory.ReadOnly = true;
+            dgvRoomDirectory.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvRoomDirectory.Size = new Size(752, 479);
+            dgvRoomDirectory.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            label6.ForeColor = SystemColors.ActiveCaptionText;
+            label6.Location = new Point(561, 8);
+            label6.Name = "label6";
+            label6.Size = new Size(176, 20);
+            label6.TabIndex = 6;
+            label6.Text = "Total Active Guests: ";
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.FromArgb(128, 128, 255);
+            panel7.Controls.Add(label1);
+            panel7.Controls.Add(label5);
+            panel7.Controls.Add(label6);
+            panel7.Location = new Point(1, 0);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(757, 34);
+            panel7.TabIndex = 4;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            label5.ForeColor = SystemColors.ActiveCaptionText;
+            label5.Location = new Point(3, 8);
+            label5.Name = "label5";
+            label5.Size = new Size(208, 20);
+            label5.TabIndex = 2;
+            label5.Text = "Lists of Guests Directory";
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(192, 192, 255);
+            panel5.Controls.Add(panel2);
+            panel5.Location = new Point(12, 63);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(333, 522);
+            panel5.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Gainsboro;
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(comboBox1);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(textBox1);
+            panel2.Location = new Point(3, 37);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(327, 482);
+            panel2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 128);
+            button1.FlatAppearance.BorderColor = Color.FromArgb(0, 192, 0);
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(13, 350);
+            button1.Name = "button1";
+            button1.Size = new Size(298, 43);
+            button1.TabIndex = 12;
+            button1.Text = "Save / Update Profile";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold);
+            label7.ForeColor = SystemColors.ActiveCaptionText;
+            label7.Location = new Point(10, 155);
+            label7.Name = "label7";
+            label7.Size = new Size(103, 15);
+            label7.TabIndex = 11;
+            label7.Text = "Email Address:";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            textBox3.Location = new Point(13, 175);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(298, 26);
+            textBox3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold);
+            label4.ForeColor = SystemColors.ActiveCaptionText;
+            label4.Location = new Point(11, 94);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Full Name:";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            textBox2.Location = new Point(13, 117);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(298, 26);
+            textBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(11, 36);
+            label3.Name = "label3";
+            label3.Size = new Size(110, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Mobile Number:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(13, 55);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 29);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(725, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 20);
+            label1.TabIndex = 8;
+            label1.Text = "52";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(13, 234);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(298, 28);
+            comboBox1.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold);
+            label8.ForeColor = SystemColors.ActiveCaptionText;
+            label8.Location = new Point(13, 216);
+            label8.Name = "label8";
+            label8.Size = new Size(59, 15);
+            label8.TabIndex = 14;
+            label8.Text = "ID Type:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold);
+            label9.ForeColor = SystemColors.ActiveCaptionText;
+            label9.Location = new Point(10, 278);
+            label9.Name = "label9";
+            label9.Size = new Size(80, 15);
+            label9.TabIndex = 16;
+            label9.Text = "ID Number:";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            textBox4.Location = new Point(13, 298);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(298, 26);
+            textBox4.TabIndex = 15;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 128);
+            button2.FlatAppearance.BorderColor = Color.FromArgb(192, 0, 0);
+            button2.FlatAppearance.BorderSize = 2;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonFace;
+            button2.Location = new Point(13, 399);
+            button2.Name = "button2";
+            button2.Size = new Size(146, 43);
+            button2.TabIndex = 17;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Silver;
+            button3.FlatAppearance.BorderColor = Color.Gray;
+            button3.FlatAppearance.BorderSize = 2;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ButtonFace;
+            button3.Location = new Point(165, 399);
+            button3.Name = "button3";
+            button3.Size = new Size(146, 43);
+            button3.TabIndex = 18;
+            button3.Text = "Clear";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(128, 255, 255);
+            button4.FlatAppearance.BorderColor = Color.FromArgb(0, 192, 192);
+            button4.FlatAppearance.BorderSize = 2;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(219, 55);
+            button4.Name = "button4";
+            button4.Size = new Size(92, 30);
+            button4.TabIndex = 19;
+            button4.Text = "Search";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // GuestLookup
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1130, 597);
+            Controls.Add(panel3);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "GuestLookup";
+            Text = "Frontdesk View";
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvRoomDirectory).EndInit();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel3;
+        private Label label2;
+        private Panel panel6;
+        private DataGridView dgvRoomDirectory;
+        private Panel panel7;
+        private Label label1;
+        private Label label5;
+        private Label label6;
+        private Panel panel5;
+        private Panel panel2;
+        private Button button1;
+        private Label label7;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button3;
+        private Button button2;
+        private Label label9;
+        private TextBox textBox4;
+        private Label label8;
+        private ComboBox comboBox1;
+        private Button button4;
+    }
+}
